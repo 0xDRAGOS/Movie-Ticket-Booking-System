89ee15f42bc458522f6c64f6b747ee87ff426626
0xDRAGOS <sdragos231@gmail.com>
2023-09-25 21:48:22 +0300

    add PrivateUser class; add PrivateUserRepository class; add loadPrivateUser method;

--- Movie Ticket Booking System/users.h
+++ Movie Ticket Booking System/users.h
@@ -125,7 +125,7 @@ PublicUser PublicUserRepository::loadPublicUser(const string& inputEmail, const 
 	PublicUser user;
 	sql::Connection* con = dbConnector.establishConnection();
 	try {
-		sql::PreparedStatement* pstmt = con->prepareStatement("SELECT * FROM users WHERE email = ? AND password = ?;");
+		sql::PreparedStatement* pstmt = con->prepareStatement("SELECT * FROM users WHERE user_type = 'public' AND email = ? AND password = ?;");
 		pstmt->setString(1, inputEmail);
 		pstmt->setString(2, inputPassword);
 		sql::ResultSet* res = pstmt->executeQuery();
@@ -138,9 +138,9 @@ PublicUser PublicUserRepository::loadPublicUser(const string& inputEmail, const 
 			getline(iss, city, ',');
 			getline(iss, street, ',');
 			getline(iss, number, ',');
-			user = PublicUser(res->getString("email"),
+			user = PublicUser(res->getString("lastName"),
 				res->getString("firstName"),
-				res->getString("lastName"),
+				res->getString("email"),
 				res->getString("password"),
 				Address(country, county, city, street, number));
 		}
@@ -176,4 +176,45 @@ void PublicUserRepository::insertIntoDatabase(PublicUser& user) {
 		exit(1);
 	}
 	dbConnector.closeConnection(con);
+}
+
+class PrivateUser : public User {
+public:
+	PrivateUser(const string& lastName = "", const string& firstName = "", const string& email = "", const string& password = "")
+		: User(lastName, firstName, email, password) {}
+};
+
+class PrivateUserRepository {
+public:
+	DatabaseConnector dbConnector;
+private:
+	PrivateUserRepository() {}
+	PrivateUser loadPrivateUser(const string& inputEmail, const string& inputPassword);
+};
+
+PrivateUser PrivateUserRepository::loadPrivateUser(const string& inputEmail, const string& inputPassword) {
+	PrivateUser user;
+	sql::Connection* con = dbConnector.establishConnection();
+	try {
+		sql::PreparedStatement* pstmt = con->prepareStatement("SELECT * FROM users WHERE user_type = 'private' AND email = ? AND password = ?;");
+		pstmt->setString(1, inputEmail);
+		pstmt->setString(2, inputPassword);
+		sql::ResultSet* res = pstmt->executeQuery();
+		while (res->next()) {
+			user = PrivateUser(res->getString("lastName"),
+				res->getString("firstName"),
+				res->getString("email"),
+				res->getString("password")
+				);
+		}
+		cout << "User loaded up successfully." << endl;
+		delete pstmt;
+		delete res;
+	}
+	catch (sql::SQLException& e) {
+		cerr << "Could not load user. Error: " << e.what() << endl;
+		exit(1);
+	}
+	dbConnector.closeConnection(con);
+	return user;
 }
